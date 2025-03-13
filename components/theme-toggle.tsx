3286ffccac9c443da847d13fe520f0e8d80aa84d
p-rer <fodosoaodosoaoso@gmail.com>
2025-03-13 15:41:05 +0900

    Enhance theme toggle button layout; add responsive text display

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -27,10 +27,13 @@ export function ThemeToggle({className}: ThemeToggleProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className={cn(className, "w-9 h-9")}>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-          <Moon
-            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+        <Button variant="ghost" size="icon" className={cn(className, "w-9 h-9 lg:w-auto p-2")}>
+          <div className="flex items-center justify-center lg:justify-start">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+            <Moon
+              className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+            <span className="hidden lg:flex ml-2">{theme?.toUpperCase()}</span>
+          </div>
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
