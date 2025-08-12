214753fca593377611ca93a1762467dcb33279fe
rer <fodosoaodosoaoso@gmail.com>
2025-08-12 14:16:33 +0900

    refactor: simplify ThemeToggle component by removing DropdownMenu and enhancing button functionality

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -26,31 +26,20 @@ export function ThemeToggle({className}: ThemeToggleProps) {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className={cn(className, "w-9 h-9 lg:w-auto p-2")}>
+        <Button variant="ghost" size="icon" className={cn(className, "w-9 h-9 lg:w-auto p-2")} onClick={() => 
+        theme === "system" ? setTheme("light") : (
+          theme === "light" ? setTheme("dark") : setTheme("system"))}>
           <div className="flex items-center justify-center lg:justify-start">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+            <Sun
+              className={theme ==="light" ? "absolute h-[1.2rem] w-[1.2rem] transition-all rotate-0 scale-100" : "absolute h-[1.2rem] w-[1.2rem] transition-all rotate-90 scale-0"}/>
             <Moon
-              className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-            <span className="hidden lg:flex ml-2">{theme?.toUpperCase()}</span>
+              className={theme ==="dark" ? "absolute h-[1.2rem] w-[1.2rem] transition-all rotate-0 scale-100" : "absolute h-[1.2rem] w-[1.2rem] transition-all rotate-90 scale-0"}/>
+            <Laptop
+              className={theme ==="system" ? "absolute h-[1.2rem] w-[1.2rem] transition-all rotate-0 scale-100" : "absolute h-[1.2rem] w-[1.2rem] transition-all rotate-90 scale-0"}/>
+            <span className="hidden lg:flex ml-6">{theme?.toUpperCase()}</span>
           </div>
-          <span className="sr-only">Toggle theme</span>
+          <span className=" sr-only">Toggle theme</span>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-2 h-4 w-4"/>
-          <span>Light</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-4 w-4"/>
-          <span>Dark</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          <Laptop className="mr-2 h-4 w-4"/>
-          <span>System</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
